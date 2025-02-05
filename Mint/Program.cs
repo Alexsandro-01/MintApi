b175018579b24b5ac7285acd6cc6619454a10203
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2025-02-05 18:14:14 -0300

    feat: Configuração da aplicação para injetar o Service e o Repository de usuários

--- Mint/Program.cs
+++ Mint/Program.cs
@@ -1,8 +1,13 @@
+using Mint.Repositories;
+using Mint.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
