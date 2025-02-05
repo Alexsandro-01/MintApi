4581cadff3e7ee4b34213dbbe73c8bcdb0e1de06
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2025-02-05 18:12:23 -0300

    feat: Criação das interfaces do repository de usuários e da interface de Service de Usuários

---
+++ Mint/Services/IUserService.cs
@@ -0,0 +1,15 @@
+using Mint.Dto;
+using Mint.Models;
+
+namespace Mint.Services
+{
+  public interface IUserService
+  {
+    User Authenticate(string email, string password);
+    UserDto CreateUser(UserDtoInsert user);
+    User GetUserById(int id);
+    User GetUserByEmail(string email);
+    void UpdateUser(User user);
+    void DeleteUser(int id);
+  }
+}
