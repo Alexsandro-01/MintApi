4c03672d0fff51cab0d4a47516f1246dcaeaf34c
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2025-02-13 18:03:20 -0300

    feat: Classe com mensagens de erro padrão

---
+++ Mint/Constants/ErrorMessages.cs
@@ -0,0 +1,11 @@
+namespace Mint.Constants
+{
+    public static class ErrorMessages
+    {
+        public const string UserNotFound = "User not found";
+        public const string UserAlreadyExists = "User already exists";
+        public const string PasswordLength = "Password must have at least 6 characters";
+        public const string NameLength = "Name must have at least 3 characters";
+        public const string InvalidEmail = "Invalid email address";
+    }
+}
