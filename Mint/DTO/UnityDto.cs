2b09be4f6dff6ef3a49093ebd0463064f7654d85
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2025-04-11 16:09:46 -0300

    feat: Unity Dto

---
+++ Mint/DTO/UnityDto.cs
@@ -0,0 +1,36 @@
+using Mint.Constants;
+
+namespace Mint.Dto;
+
+public class UnityDto
+{
+  public int UnityId { get; set; }
+  public string Name { get; set; }
+  public DateTime Created_at { get; set; }
+}
+
+public class UnityDtoInsert
+{
+  public int? UnityId { get; set; }
+  public string Name { get; set; }
+
+  public List<string> Validate()
+  {
+    List<string> errors = new();
+
+    if (string.IsNullOrEmpty(Name) || Name.Length < 2)
+    {
+      errors.Add(ErrorMessages.InvalidUnityNameLength);
+    }
+
+    return errors;
+  }
+}
+
+public class UnityDtoResponse
+{
+  public bool Success { get; set; }
+  public string Message { get; set; }
+  public UnityDto[]? Unity { get; set; }
+}
+
