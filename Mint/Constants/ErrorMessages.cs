9f57078ca0c6489b7bd3dc04df78daf09e86ee4e
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2025-06-02 17:50:40 -0300

    chore: Adiciona novas mensagens

--- Mint/Constants/ErrorMessages.cs
+++ Mint/Constants/ErrorMessages.cs
@@ -1,20 +1,21 @@
 namespace Mint.Constants
 {
-    public static class ErrorMessages
-    {
-        public const string UserNotFound = "User not found";
-        public const string UserAlreadyExists = "User already exists";
-        public const string PasswordLength = "Password must have at least 6 characters";
-        public const string NameLength = "Name must have at least 3 characters";
-        public const string InvalidEmail = "Invalid email address";
+  public static class ErrorMessages
+  {
+    public const string UserNotFound = "User not found";
+    public const string UserAlreadyExists = "User already exists";
+    public const string PasswordLength = "Password must have at least 6 characters";
+    public const string NameLength = "Name must have at least 3 characters";
+    public const string InvalidEmail = "Invalid email address";
 
-        public const string InvalidEmailOrPassword = "Invalid email or password";
+    public const string InvalidEmailOrPassword = "Invalid email or password";
 
-        // Unity
-        public const string InvalidUnityNameLength = "Unity name must have at least 2 characters";
+    // Unity
+    public const string InvalidUnityNameLength = "Unity name must have at least 2 characters";
 
-        // Product
-        public const string InvalidProductNameLength = "Product name must have at least 3 characters";
-        public const string InvalidProductCodeLength = "Product code must have at least 3 characters";
+    // Product
+    public const string InvalidProductNameLength = "Product name must have at least 3 characters";
+    public const string InvalidProductCodeLength = "Product code must have at least 3 characters";
+    public const string ProductNotFound = "Product not found";
     }
 }
--- Mint/Constants/SuccesMessages.cs
+++ Mint/Constants/SuccesMessages.cs
@@ -1,27 +1,32 @@
 namespace Mint.Constants
 {
-    public static class SuccesMessages
-    {
-        public const string UserCreated = "User created successfully";
-        public const string UserFound = "User found successfully";
-        public const string UserUpdated = "User updated successfully";
-        public const string UserDeleted = "User deleted successfully";
-        public const string TokenGenerated = "Token generated successfully";
+  public static class SuccesMessages
+  {
+    public const string UserCreated = "User created successfully";
+    public const string UserFound = "User found successfully";
+    public const string UserUpdated = "User updated successfully";
+    public const string UserDeleted = "User deleted successfully";
+    public const string TokenGenerated = "Token generated successfully";
 
-        // Customer
-        public const string CustomerCreated = "Customer created successfully";
-        public const string CustomerFound = "Customer found successfully";
-        public const string CustomerUpdated = "Customer updated successfully";
-        public const string CustomerDeleted = "Customer deleted successfully";
+    // Customer
+    public const string CustomerCreated = "Customer created successfully";
+    public const string CustomerFound = "Customer found successfully";
+    public const string CustomerUpdated = "Customer updated successfully";
+    public const string CustomerDeleted = "Customer deleted successfully";
 
-        // Supplier
-        public const string SupplierCreated = "Supplier created successfully";
-        public const string SupplierFound = "Supplier found successfully";
-        public const string SupplierUpdated = "Supplier updated successfully";
-        public const string SupplierDeleted = "Supplier deleted successfully";
+    // Supplier
+    public const string SupplierCreated = "Supplier created successfully";
+    public const string SupplierFound = "Supplier found successfully";
+    public const string SupplierUpdated = "Supplier updated successfully";
+    public const string SupplierDeleted = "Supplier deleted successfully";
 
-        // Unit
-        public const string UnitFound = "Unit found successfuly";
-        public const string UnitCreated = "Unit created successfully";
+    // Unit
+    public const string UnitFound = "Unit found successfuly";
+    public const string UnitCreated = "Unit created successfully";
+
+    // Product
+    public const string ProductCreated = "Product created successfully";
+    public const string ProductFound = "Product found successfuly";
+
     }
 }
