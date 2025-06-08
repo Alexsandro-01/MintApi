6af697d5578bbd94d97267ecc22f4607b1ed7086
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2025-06-08 13:44:28 -0300

    feat: Adiciona mensagens de erro da rota Entry

--- Mint/Constants/ErrorMessages.cs
+++ Mint/Constants/ErrorMessages.cs
@@ -2,6 +2,7 @@ namespace Mint.Constants
 {
   public static class ErrorMessages
   {
+    // User
     public const string UserNotFound = "User not found";
     public const string UserAlreadyExists = "User already exists";
     public const string PasswordLength = "Password must have at least 6 characters";
@@ -12,10 +13,21 @@ namespace Mint.Constants
 
     // Unity
     public const string InvalidUnityNameLength = "Unity name must have at least 2 characters";
+    public const string UnityNotFound = "Unity not found";
 
     // Product
     public const string InvalidProductNameLength = "Product name must have at least 3 characters";
     public const string InvalidProductCodeLength = "Product code must have at least 3 characters";
     public const string ProductNotFound = "Product not found";
-    }
+
+    // Entry
+    public const string QuantityInvalid = "Quantity must be greater than 0";
+    public const string CostInvalid = "Cost must be a non-negative value";
+    public const string EntryNotFound = "Entry not found";
+    public const string EntryNotFoundInDateRange = "Entry not found in the specified date range";
+
+    // Customer
+    public const string CustomerNotFound = "Customer not found";
+    public const string InvalidCustomerNameLength = "Customer name must have at least 3 characters";
+  }
 }
