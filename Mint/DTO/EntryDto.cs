fdda37d88b8fa3c6070763aa4d85d015402352a2
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2025-06-09 19:30:54 -0300

    feat: Adiciona novos campos

--- Mint/DTO/EntryDto.cs
+++ Mint/DTO/EntryDto.cs
@@ -1,4 +1,5 @@
 using Mint.Constants;
+using Mint.Models;
 
 namespace Mint.DTO;
 
@@ -7,6 +8,9 @@ public class EntryDto
   public int Id { get; set; }
   public int UserId { get; set; }
   public int ProductId { get; set; }
+  public Product? Product { get; set; }
+  public Unit? Unit { get; set; }
+  public Customer? Customer { get; set; }
   public int UnitId { get; set; }
   public int CustomerId { get; set; }
   public double Quantity { get; set; }
@@ -19,12 +23,13 @@ public class EntryDto
 public class EntryDtoInsert
 {
   public int? Id { get; set; }
-  public int UserId { get; set; }
+  public int? UserId { get; set; }
   public int ProductId { get; set; }
   public int UnitId { get; set; }
   public int CustomerId { get; set; }
   public double Quantity { get; set; }
   public int Cost { get; set; }
+
   public DateTime SaleDate { get; set; }
 
   public List<string> Validate()
